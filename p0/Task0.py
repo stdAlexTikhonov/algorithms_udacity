94041392547d12e76942797610cf26d35936ead7
Aleksandr Tihonov <a.tihonov@polymatica.com>
2021-05-30 23:19:12 +0300

    fix(Task0) use format

--- p0/Task0.py
+++ p0/Task0.py
@@ -20,8 +20,5 @@ Print messages:
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
 
-print("First record of texts, " + texts[0][0] +  " texts " + texts[0][1] + " at time " + texts[0][2])
-
-
-last_call = len(calls) - 1
-print("Last record of calls, " + calls[last_call][0] +  " calls " + calls[last_call][1] + " at time " + calls[last_call][2] + " , lasting " + calls[last_call][3] + " seconds")
+print('First record of texts, {0} texts {1} at time {2}'.format(*texts[0]))
+print('Last record of calls, {0} calls {1} at time {2}, lasting {3} seconds'.format(*calls[-1]))
