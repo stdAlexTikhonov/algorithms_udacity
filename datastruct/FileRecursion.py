2ed4c03cd6d9cf7c66605231c5791c0d528ee705
Aleksandr Tihonov <a.tihonov@polymatica.com>
2021-07-01 12:45:22 +0300

    FileRecursion added tst cases

--- datastruct/FileRecursion.py
+++ datastruct/FileRecursion.py
@@ -2,14 +2,27 @@ import os
 
 def find_files(suffix, path):
     output = []
-    items_in_dir = os.listdir(path)
-    for item in items_in_dir:
-        if os.path.isfile(path + "/" + item):
-            if item.endswith(suffix):
-                output.append(item)
-        else:
-            output.extend(find_files(suffix,path + '/' + item))
-    return output
+    if os.path.exists(path):
+        items_in_dir = os.listdir(path)
+        if len(items_in_dir) == 0:
+            print("Dir {} is empty".format(path))
+            return None
+        for item in items_in_dir:
+            if os.path.isfile(path + "/" + item):
+                if item.endswith(suffix):
+                    output.append(item)
+            else:
+                output.extend(find_files(suffix,path + '/' + item))
+        return output
+    else:
+        print("Path {} doesnt exists".format(path))
+        return None
 
 
 print(find_files(".c", "./testdir"))
+
+#Test not exists
+print(find_files(".c", "./other"))
+
+#Test empty dir
+print(find_files(".c", "./empty"))
