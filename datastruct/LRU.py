4fd6c38ddb869b31b4b3ec346ccbf432fceee61b
Aleksandr Tihonov <a.tihonov@polymatica.com>
2021-07-01 12:37:13 +0300

    Added tests for LRU

--- datastruct/LRU.py
+++ datastruct/LRU.py
@@ -40,37 +40,59 @@ class LRU_Cache(object):
             return -1
 
     def set(self, key, value):
-        self.cache[key] = value
-        new_node = Node(value, key)
-        if self.linked_list.size < self.capacity:
-            self.linked_list.add(new_node)
+        if self.capacity and self.capacity > 0:
             self.cache[key] = value
+            new_node = Node(value, key)
+            if self.linked_list.size < self.capacity:
+                self.linked_list.add(new_node)
+                self.cache[key] = value
+            else:
+                first_item = self.linked_list.remove_first()
+                del self.cache[first_item.key]
+                self.linked_list.add(new_node)
+                self.cache[key] = value
         else:
-            first_item = self.linked_list.remove_first()
-            del self.cache[first_item.key]
-            self.linked_list.add(new_node)
-            self.cache[key] = value
+            print("Cannot set cache with capacity of {}".format(self.capacity))
 
 
 
 
-
+#Test1
 our_cache = LRU_Cache(5)
 
-our_cache.set(1, 1);
-our_cache.set(2, 2);
-our_cache.set(3, 3);
-our_cache.set(4, 4);
+our_cache.set(1, 1)
+our_cache.set(2, 2)
+our_cache.set(3, 3)
+our_cache.set(4, 4)
 
 
 print(our_cache.get(1))       # returns 1
 print(our_cache.get(2))       # returns 2
 print(our_cache.get(9))      # returns -1 because 9 is not present in the cache
 
-our_cache.set(5, 5) 
-our_cache.set(6, 6)
 
-print(our_cache.get(3))
+#Test2 0
+cache2 = LRU_Cache(0)
+cache2.set(1, 1)
+print(cache2.get(1))
+
+#Test2 -1
+cache2 = LRU_Cache(-1)
+cache2.set(1, 1)
+print(cache2.get(1))
+
+#Test3 High capacity
+capacity = 100
+chache3 = LRU_Cache(capacity)
+capacity += 10
+
+for i in range(capacity + 1):
+    chache3.set(i, i)
+
+print(chache3.get(capacity))
+
+
+
 
 """
 To solve this problem I used LinkedList and built-in dict structure.
