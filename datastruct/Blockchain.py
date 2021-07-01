fcb7246600a6127929f4bce2193b85221c4b5e6d
Aleksandr Tihonov <a.tihonov@polymatica.com>
2021-07-01 14:08:43 +0300

    Test Cases fro Blockchain

--- datastruct/Blockchain.py
+++ datastruct/Blockchain.py
@@ -22,30 +22,52 @@ class Blockchain:
         self.tail = None
         self.size = 0
 
-    def add(self, node):
-        if not self.root:
-            self.root = node
-            self.tail = self.root
+    def add_block(self, data = None):
+        if data:
+            if not self.root:
+                node = Block(data)
+                self.root = node
+                self.tail = self.root
+            else:
+                node = Block(data, self.tail.hash)
+                if node.timestamp == self.tail.timestamp:
+                    print("cannoot insert block with same timestamp")
+                    return False
+                self.tail.next = node
+                self.tail = self.tail.next
+            self.size += 1
         else:
-            self.tail.next = node
-            self.tail = self.tail.next
+            print("Data is Empty")
+            return None
+
+    def print(self):
+        if self.root:
+            curr = self.root
+            print("Blockchain: ")
+            while curr:
+                print(" timestamp: {}, data: {}".format(curr.timestamp, curr.data))
+                curr = curr.next
+        pass
 
 
-block1 = Block(100)
-block2 = Block(200, block1.hash)
-block3 = Block(300, block2.hash)
-block4 = Block(400, block3.hash)
-block5 = Block(500, block4.hash)
+
 
 blockchain = Blockchain()
 
-blockchain.add(block1)
-blockchain.add(block2)
-blockchain.add(block3)
-blockchain.add(block4)
-blockchain.add(block5)
+#Test insert block with same timestamp
+blockchain.add_block(100)
+blockchain.add_block(200)
 
+#Test insert empty data
+time.sleep(.5)
+blockchain.add_block()
 
+time.sleep(.8)
+blockchain.add_block(400)
+time.sleep(1)
+blockchain.add_block(500)
+
+blockchain.print()
 
 
 
