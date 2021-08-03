db5c7a75416789b43e8ee8a2ac74f15a889337f9
Aleksandr Tihonov <a.tihonov@polymatica.com>
2021-08-03 21:48:47 +0300

    Minmax new

--- Algorithms/problem6.py
+++ Algorithms/problem6.py
@@ -37,10 +37,49 @@ tests = [
     [1,90,23,14,23,212,0]
 ]
 
-def test(tests):
+def test(tests, fn):
     for item in tests:
-        minmax = find_minmax(item)
-        print("max: {} min: {}".format(minmax.max, minmax.min))
+        minmax = fn(item)
+        if type(minmax) is tuple:
+             print("max: {} min: {}".format(minmax[1], minmax[0]))
+        else:
+             print("max: {} min: {}".format(minmax.max, minmax.min))
+       
 
 
-test(tests)
+test(tests, find_minmax)
+
+
+def quick_sort(sequence):
+    length = len(sequence)
+    if length <= 1:
+        return sequence
+    else:
+        pivot = sequence.pop()
+
+    items_greater = []
+    items_lower = []
+
+    for item in sequence:
+        if item > pivot:
+            items_greater.append(item)
+        else:
+            items_lower.append(item)
+
+    return quick_sort(items_lower) + [pivot] + quick_sort(items_greater)
+
+def quick_minmax(arr):
+    if len(arr) == 0:
+        return (None, None)
+
+    if len(arr) == 1:
+        return (arr[0], arr[0])
+
+    sorted_arr = quick_sort(arr)
+    length = len(sorted_arr)
+
+    return (sorted_arr[0], sorted_arr[length-1] )
+
+test(tests, quick_minmax)
+
+    
