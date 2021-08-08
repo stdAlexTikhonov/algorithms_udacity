ca57b53d5c1079fdcbf52f183c8fee34903667d7
Aleksandr Tihonov <a.tihonov@polymatica.com>
2021-08-09 00:35:25 +0300

    edge cases for problem 1

--- Algorithms/problem1.py
+++ Algorithms/problem1.py
@@ -1,17 +1,35 @@
+import math
+
 def sqrt(k):
     low = 0
     high = k
+    if k > 0:
+        while low <= high:
+            mid = (low + high) // 2
+            mid_squared = mid * mid
 
-    while low <= high:
-        mid = (low + high) // 2
-        mid_squared = mid * mid
+            if mid_squared <= k:
+                low = mid + 1
+            else:
+                high = mid - 1
+            
+        return low - 1
+    else:
+        print('Input is less or equal to 0')
+        return -1
 
-        if mid_squared <= k:
-            low = mid + 1
-        else:
-            high = mid - 1
-        
-    return low - 1
+def test_function(test_case, ans):
+
+    if sqrt(test_case) == ans:
+        print("Pass")
+    else:
+        print(sqrt(test_case))
+        print("Fail")
+
+test_function(300, 17)
+test_function(-1, -1)
+test_function(0, -1)
+
 
 # in naive approach To find squared number of k we have to go from 0 all the way to k, and check result of number ^ 2
 # with binary search we can do it faster
