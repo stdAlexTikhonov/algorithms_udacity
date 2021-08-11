70546ca7c87c930331539120e9d7decd246d581a
Aleksandr Tihonov <a.tihonov@polymatica.com>
2021-08-12 00:37:21 +0300

    Added graph

---
+++ Pathfinding/deikstra.py
@@ -0,0 +1,39 @@
+from collections import defaultdict
+
+class Graph:
+    def __init__(self):
+        self.nodes = set()
+        self.neighbours = defaultdict(list)
+        self.distances = {}
+
+    def add_node(self, value):
+        self.nodes.add(value)
+
+    def add_edge(self, from_node, to_node, distance):
+        self.neighbours[from_node].append(to_node)
+        self.neighbours[to_node].append(from_node)
+        self.distances[(from_node, to_node)] = distance
+        self.distances[(to_node, from_node)] = distance
+
+    def print_graph(self):
+        print("Set of Nodes are: ", self.nodes)
+        print("Neighbours are: ", self.neighbours)
+        print("Distances are: ", self.distances)
+
+
+testGraph = Graph()
+
+for node in ['A', 'B', 'C', 'D', 'E']:
+    testGraph.add_node(node)
+
+testGraph.add_edge('A','B',3)
+testGraph.add_edge('A','D',2)
+testGraph.add_edge('B','D',4)
+testGraph.add_edge('B','E',6)
+testGraph.add_edge('B','C',1)
+testGraph.add_edge('C','E',2)
+testGraph.add_edge('E','D',1)
+
+
+def deikstra(graph, source):
+    pass
